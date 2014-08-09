e0d8d97399a6a3f40f425bc8b4f517b8e756b188
Felix Gundlack <GundlackFelixDEV@gmail.com>
2014-08-09 13:30:11 +0200

    Initial Project

---
+++ MongoTodo/MongoTodo/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoTodo.Models
+{
+    public class Todo
+    {
+        public Todo()
+        {
+            Id = -1;
+        }
+        public string Titel { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public int Id { get; set; }
+        public DateTime Create { get; set; }
+        public DateTime Change { get; set; }
+        public DateTime Due { get; set; }
+    }
+}
---
+++ MongoTodo/MongoTodo/Models/TodoManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MongoTodo.Models
+{
+    public class TodoManager
+    {
+        private static List<Todo> todoItems;
+
+        public TodoManager()
+        {
+            if (todoItems == null)
+                todoItems = new List<Todo>();
+
+        }
+
+        public List<Todo> allItems
+        {
+            get
+            {
+                return todoItems;
+            }
+        }
+
+        public void newItem(Todo aItem)
+        {
+            var newId = (todoItems.Count == 0)?1:todoItems.Max(t => t.Id);
+            aItem.Id = newId;
+            todoItems.Add(aItem);
+        }
+
+        public Todo findItem(int id)
+        {
+            return todoItems.FirstOrDefault(i => i.Id == id);
+        }
+
+        public void editItem(Todo aItem)
+        {
+            Todo item = findItem(aItem.Id);
+            if(item != null)
+            {
+                item.Description = aItem.Description;
+                item.Titel = aItem.Titel;
+                item.Due = aItem.Due;
+                item.Priority = aItem.Priority;
+                item.Change = DateTime.Now;
+            }
+            else
+            {
+                throw(new InvalidOperationException(string.Format("Item {0} not found!",aItem.Id));
+            }
+        }
+
+
+    }
+}
